7e49d6f8504ca031a17641d2b57e1a36fca4f841
Vitor Correa <vitorcorrea18@gmail.com>
2021-11-19 19:31:08 -0400

    exercicio 7 completo

--- fundamentos/bloco-08-ES6-introdução-higher-order-functions/dia-03/exercicio7.js
+++ fundamentos/bloco-08-ES6-introdução-higher-order-functions/dia-03/exercicio7.js
@@ -1,7 +1,5 @@
 const books = require('./books');
 
-
-
 /* 7 - Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
 Dica: cada inicial termina com um ponto. */
 
@@ -9,5 +7,9 @@ Dica: cada inicial termina com um ponto. */
 const expectedResult = 'O Senhor dos Anéis';
 
 function authorWith3DotsOnName() {
-  // escreva seu código aqui
+  return books.find((book) => (
+    book.author.name.split(' ').filter((word) => word.endsWith('.')).length === 3
+  )).name;
 }
+
+console.log(authorWith3DotsOnName());
